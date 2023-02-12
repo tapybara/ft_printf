c521e9743f4d7cbc78679d6c2aff81fd45d4521d
tokuyama <okuyama0529work@gmail.com>
2023-02-12 15:26:36 +0900

    Add flag-mode for %s

--- srcs/ft_print_utils_str.c
+++ srcs/ft_print_utils_str.c
@@ -6,22 +6,50 @@
 /*   By: okuyamatakahito <okuyamatakahito@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 21:33:29 by okuyamataka       #+#    #+#             */
-/*   Updated: 2023/02/08 22:04:56 by okuyamataka      ###   ########.fr       */
+/*   Updated: 2023/02/12 15:24:29 by okuyamataka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../includes/ft_printf.h"
 
-void	ft_print_char(t_print *tab, int c)
+int	ft_putchar(int c)
 {
-	tab->tl += write(STDOUT_FILENO, &c, 1);
+	return (write(STDOUT_FILENO, &c, 1));
+}
+
+int	ft_putstr(char *str)
+{
+	return (write(STDOUT_FILENO, str, ft_strlen(str)));
 }
 
 void	ft_print_str(t_print *tab, char *str)
 {
+	int	len;
+
 	if (!str)
-		ft_print_str(tab, "(null)");
-	else
-		tab->tl += write(STDOUT_FILENO, str, ft_strlen(str));
+	{
+		tab->tl += ft_putstr("(null)");
+		return ;
+	}
+	else if (is_empty_char_required(tab, true))
+		return ;
+	len = (int)ft_strlen(str);
+	ft_print_str_with_flags(tab, str, len);
+	// ft_print_nbr_with_flags(tab, str, len);
+}
+
+void	ft_print_str_with_flags(t_print *tab, char *str, int len)
+{
+	int	print_len;
+	
+	if (tab->prec < len && tab->dot)
+		len = tab->prec;
+	print_len = len;
+	if (!tab->dash)
+		fill_the_margin(tab, len);
+	while (print_len--)
+		tab->tl += ft_putchar(*str++);
+	if (tab->dash)
+		fill_the_margin(tab, len);
 }
